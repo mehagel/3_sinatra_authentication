5ac3e83f1a1c6515c7e9a73cb2e87a626d8929bf
Apprentice <apprentice@devbootcamp.com>
2013-07-27 17:02:27 -0700

    Fixed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,8 @@ require 'faker'
 #TODO: Once you have implemented BCrypt - you can use these to seed your database.
 
 
-# User.create :name => 'Dev Bootcamp Student', :email => 'me@example.com', :password => 'password'
-# 5.times do
-#   User.create :name => Faker::Name.name, :email => Faker::Internet.email, :password => 'password'
-# end
+User.create :name => 'Dev Bootcamp Student', :email => 'me@example.com', :password_hash => 'password'
+5.times do
+  @password = BCrypt::Password.create 'password'
+  User.create :name => Faker::Name.name, :email => Faker::Internet.email, :password_hash => @password
+end
