e460fc587a9d29594bacd0261df9525922c6850e
mehagel65@gmail.com <mehagel65@gmail.com>
2013-08-13 20:30:54 -0700

    cleaned up files added faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,6 @@
 require 'faker'
 
-# create a few users
-
-#TODO: Once you have implemented BCrypt - you can use these to seed your database.
-
-
-User.create :name => 'Dev Bootcamp Student', :email => 'me@example.com', :password_hash => 'password'
-5.times do
+10.times do
   @password = BCrypt::Password.create 'password'
   User.create :name => Faker::Name.name, :email => Faker::Internet.email, :password_hash => @password
 end
